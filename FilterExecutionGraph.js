13b6dfe0a3b79c6c5912822d0ef7f81d17f615bc
Henry <ht17dk@brocku.ca>
2020-10-26 21:46:31 -0400

    Completed not working protype

--- FilterExecutionGraph.js
+++ FilterExecutionGraph.js
@@ -4,167 +4,139 @@ loadModule("/TraceCompass/DataProvider")
 loadModule("/TraceCompass/View")
 loadModule("/TraceCompass/Utils");
 
-time1 = 0;
-time2 = 9999999999999999999999999;
-tid = "";
+//Filter values
+const time1 = 0;                            				//start time
+const time2 = 1602814327606576000;          				//end time
+const filterTid = -1;                     					//tid: -1 if not in use
+const filterStatus = "none";                  				//status: none if not in use
+const saveLocation = "workspace://DataFiltering/output.txt";
 
-filterOsExecutionGraph(time1, time2, tid);
+/* 
+Possible statuses:
 
-
-function filterGraph(next, graph, ss, time1, time2, tid){
-	//Get the ENUM of the edge directions
-	edges =	org.eclipse.tracecompass.analysis.graph.core.base.TmfVertex.EdgeDirection.values();
-	//Get the Enum of the statuses
-	statuses = org.eclipse.tracecompass.analysis.os.linux.core.model.ProcessStatus.values();
-	// Associate a TID with a graph worker
-	tidToWorkerMap = {};
-	//For holding pending arrows
-	pendingArrows = {};
-	//For saving the arrow information
-	arrows = [];
-	
-	//Loop for all nodes in graph
-	while(next != null){
-		vertex = next;
-		vertical = false;
-		//outputStr = "";
-		
-		//Find next vertex
-		if(vertex.getEdge(edges[0]) != null){
-			edge = vertex.getEdge(edges[0]);
-			next = vertex. getNeighborFromEdge(edge, edges[0]);
-			vertical = true;
-		}else if (vertex.getEdge(edges[2]) != null){
-			edge = vertex.getEdge(edges[2]);
-			next = vertex. getNeighborFromEdge(edge, edges[2]);
-		}else{
-			next = null;
-		}
-		
-		sTime = vertex.getTs();
-		
-		//check if this node is within filter
-		if((sTime >= time1) && (sTime <= time2)){
-			//Get data of vertex and node
-			worker = graph.getParentOf(vertex);
-			name = worker.getName();
-			//info = worker.getWorkerInformation(sTime);
-			tid = worker.getHostId();
-			id = vertex.getID();
-			
-			
-			
-			//statusNum = worker.getStatus().getStateValue().unboxInt();
-			//status = statuses[statusNum];
-			//prevStatus = worker.getOldStatus();
-			
-			//output data
-			///outputStr += "Worker of Vertex: " + name + "\nWorker info: " + info + "\nStatus: " + status + "\nOld status: " + prevStatus + "Vertex start time: " + sTime;
-			if(next != null){
-				eTime = next.getTs()-1;
-				elapsed = eTime - sTime;
-				
-				//outputStr += "\nVertex end time: " + eTime + "\nTime elapsed: " + elapsed;
-				if(vertical){
-					//outputStr += "\nSwitch to different worker";
-				}
-			}
-		}
-		if(next == null){
-			//outputStr += "\nEnd of Crit Path";
-		}
-		//writeLine(fileHandle, outputStr + "\n");
-	}
-}
-
-//provider = new org.eclipse.tracecompass.analysis.os.linux.core.execution.graph.OsExecutionGraphProvider(trace);
-
-
-
-//var ss = analysis.getStateSystem(false);
-
-
-
-/*
-
-var event = null
-
-while (iter.hasNext()) {
-	event = iter.next();
-	eventName = event.getName();
-	tid = getEventFieldValue(event, "context._vtid");
-	worker_id = getEventFieldValue(event, "worker_id");
-	tidToWorkerMap[tid] = worker_id;
-		
-}
-
-if (event != null) {
-	ss.closeHistory(event.getTimestamp().toNanos());
-}
-
-
-var tgEntries = createListWrapper();
-var mpiWorkerToId = {};
-for (i = 0; i < quarks.size(); i++) {
-	quark = quarks.get(i);
-	// Get the mpi worker ID, and find its quark
-	mpiWorkerId = ss.getAttributeName(quark);
-	// Create an entry with the worker ID as name and the quark. The quark will be used to populate the entry's data.
-	entry = createEntry(mpiWorkerId, {'quark' : quark});
-	mpiWorkerToId[mpiWorkerId] = entry.getID();
-	mpiEntries.push(entry);
-}
-// Sort the entries numerically
-mpiEntries.sort(function(a,b){return Number(a.getName()) - Number(b.getName())});
-// Add the entries to the entry list
-for (i = 0; i < mpiEntries.length; i++) {
-	tgEntries.getList().add(mpiEntries[i]);
-}
-function getEntries(parameters) {
-	// The list is static once built, return all entries
-	return tgEntries.getList();
-}
-provider = createScriptedTimeGraphProvider(analysis, getEntries, null, null);
-if (provider != null) {
-	// Open a time graph view displaying this provider
-	openTimeGraphView(provider);
-}
+	BLOCK_DEVICE
+	BLOCKED
+	DEFAULT
+	EPS
+	INTERRUPTED
+	IPI
+	NETWORK
+	PREEMPTED
+	RUNNING
+	TIMER
+	UNKNOWN
+	USER_INPUT
+See: 
+https://archive.eclipse.org/tracecompass/doc/javadoc/apidocs/org/eclipse/tracecompass/analysis/graph/core/base/TmfEdge.EdgeType.html
+for details
 */
 
 
-
-function filterOsExecutionGraph(time1, time2, tid){
+function filterExeGraph(){
 	//Get the active trace
 	var trace = getActiveTrace();
 	if (trace == null) {
-	      print("Trace is null");
-	      exit();
+		print("Trace is null");
+		exit();
 	}
-	
+		
 	// Get the Statistics module (by name) for that trace
 	var analysis = getTraceAnalysis(trace, 'OS Execution Graph');
 	if (analysis == null) {
-	      print("Statistics analysis not found");
-	      exit();
+		print("Statistics analysis not found");
+		exit();
 	}
-	//get state system
-	ss = createScriptedAnalysis(trace, "FilterExecutionGraph.js").getStateSystem(false);
 	var graph = analysis.getGraph();
-	
 	if(graph == null){
 		print("Os Execution Graph not found");
-	     exit();
+		exit();
 	}
 	
-	var tidToWorkerMap = {};
-
-	print("graph " + graph);
-
-	var workers = graph.getWorkers();
+	//get head of graph
 	head = graph.getHead();
-	next = head;
+	if(head == null){
+		print("Head vertex not found");
+		exit();
+	}
 	
-	filterGraph(next, graph, ss, time1, time2, tid);
+	//Get the ENUM of the edge directions
+	var edges =	org.eclipse.tracecompass.analysis.graph.core.base.TmfVertex.EdgeDirection.values();
+	
+	//Get state system:
+	var scriptedAnalysis = createScriptedAnalysis(trace, "FilterExecutionGraph.js");
+	var ss = scriptedAnalysis.getStateSystem(false);
+	
+	//For graph output
+	var tidToWorkerMap = {};
+	
+	var headTime = head.getTs();
+	var graphEndTime = 0;
+	
+	filter(head);
+	
+	
+	ss.closeHistory(graphEndTime);
+	
+	//output graph
+	var map = new java.util.HashMap();
+	map.put(ENTRY_PATH, '*');
+	print(map);
+	
+	provider = createTimeGraphProvider(scriptedAnalysis, map, "");
+	print(analysis.getClass());
+	
+	if (provider != null) {
+		print("provider not null");
+		// Open a time graph view displaying this provider
+		openTimeGraphView(provider);
+	}else{
+		print("provider is null");
+	}
 
+}
+
+/////////////////////////////////////////
+function filter(vertex){
+	horEdge = vertex.getEdge(edges[2]);
+	verEdge = vertex.getEdge(edges[0]);
+	worker = graph.getParentOf(vertex);
+	name = worker.getName();
+	sTime = vertex.getTs();
+	info = worker.getWorkerInformation(sTime);
+	tid = info.get('TID');
+	within = true;
+	
+	//check if this node is within filter
+	if(!((sTime >= time1) && (sTime <= time2)) || ((status != "none") && (status != type)) || ((filterTid != -1) && (filterTid != tid) ) || (next== null)){
+		within = false;
+	}
+	
+	
+	//base case
+	if( (horEdge == null) && (verEdge == null) ){
+		return;
+	}
+	if(verEdge != null){
+		verNext = vertex.getNeighborFromEdge(verEdge, edges[0]);
+		filter(verNext);
+	}
+	if(horEdge != null){
+		if(within){
+			horNext = vertex.getNeighborFromEdge(horEdge, edges[2]);
+			type = horEdge.getType();
+			eTime = horNext.getTs();
+			if(eTime > graphEndTime){
+				graphEndTime = eTime;
+			} 
+			tidToWorkerMap[tid] = name;
+			status = horEdge.getType();
+			quark = ss.getQuarkAbsoluteAndAdd(name);
+			ss.modifyAttribute(sTime, status.toString(), quark);
+			ss.removeAttribute(eTime, quark);
+		}
+		filter(horNext);
+	}
+	
+	return;
 }
 
